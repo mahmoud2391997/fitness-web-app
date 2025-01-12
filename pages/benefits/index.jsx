43195ad178c1fd32c6dd5d32953bfcc516bb5244
mahmoud2391997 <mahmoudmelsaid1@gmail.com>
2025-01-12 21:59:31 +0200

    benefet page

---
+++ pages/benefits/index.jsx
@@ -0,0 +1,73 @@
+function Index(params) {
+    return <div>
+    <section>
+      <img src="/header.png" alt="benefits" />
+      <div className="bg-transparent text-white p-6 flex  justify-evenly bg-[#343338]">
+   <ul className="space-y-4 list-inside w-1/3 ">
+     <li>
+       <h3 className="font-semibold">Targeted Muscle Development:</h3>
+       <p>
+         It effectively isolates the calf muscles, particularly the gastrocnemius and soleus.
+       </p>
+     </li>
+     <li>
+       <h3 className="font-semibold">Versatile Training Options:</h3>
+       <p>
+         It can be used with various weights, allowing for progression and adaptation as your strength increases.
+       </p>
+     </li>
+     <li>
+       <h3 className="font-semibold">Reduced Risk of Injury:</h3>
+       <p>
+         With proper form and technique, the attachment can help prevent common calf injuries.
+       </p>
+     </li>
+     <li>
+       <h3 className="font-semibold">Space-Saving Design:</h3>
+       <p>
+         It’s very compact and can be easily stored when not in use, making it convenient for at-home workouts.
+       </p>
+     </li>
+     <li>
+       <h3 className="font-semibold">Comfortable and Ergonomic:</h3>
+       <p>
+         The padded support helps reduce strain on the lower back and ensures a more comfortable workout experience.
+       </p>
+     </li>
+   </ul>
+   <ul className="space-y-4 list-inside w-1/3">
+     <li>
+       <h3 className="font-semibold">Improved Range of Motion:</h3>
+       <p>
+         The design allows for a deeper stretch and contraction, enhancing flexibility and muscle strength.
+       </p>
+     </li>
+     <li>
+       <h3 className="font-semibold">Injury Prevention:</h3>
+       <p>
+         Strengthening the calf muscles helps stabilize and balance during everyday activities and athletic movements.
+       </p>
+     </li>
+     <li>
+       <h3 className="font-semibold">Better Balance and Stability:</h3>
+       <p>
+         Strong calf muscles contribute to better balance and stability, which is crucial for maintaining proper posture during various activities.
+       </p>
+     </li>
+     <li>
+       <h3 className="font-semibold">Enhanced Athletic Performance:</h3>
+       <p>
+         Athletes, particularly those involved in sports like basketball, soccer, and sprinting, benefit from strong calf muscles, which provide bursts of power and speed.
+       </p>
+     </li>
+   </ul>
+ </div>
+ 
+     </section>
+    
+     <img src="/benefets.jpeg" alt="benefits" />
+     <img src="/bg-sec5.jpeg" alt="benefits" />
+    
+    </div>    
+ }
+ export default Index;
